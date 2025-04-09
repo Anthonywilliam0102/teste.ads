fc646934d4054a56ca8d9c2c1f9cf8ba9d2da98a
Anthonywilliam0102 <anthonymotaw01@gmail.com>
2025-04-09 11:27:59 -0300

    alteração da soma

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
-print ("ESTA É UMA CALCULADORA")
+def somar(a, b):
+    return a + b
+
+try:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = somar(num1, num2)
+    print(f"O resultado da soma de {num1} + {num2} é: {resultado}")
+except ValueError:
+    print("Por favor, insira números válidos.")
