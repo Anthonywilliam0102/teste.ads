f3f0f56f727e6cf136238c2fe77b0fc1fe3f611d
unknown <fellipefragoso23@gmail.com>
2025-04-29 22:13:47 -0300

    Adiciona operação de multiplicação

---
+++ multiplicacao.py
@@ -0,0 +1,6 @@
+def multiplicacao(a, b):
+    return a * b
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+print(f"A multiplicação é: {multiplicacao(num1, num2)}")
